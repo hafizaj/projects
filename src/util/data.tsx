3a7b1440a215decbe908a09e7785d02677ed7615
Hafizuddin <hafizuddinbinjaafar@gmail.com>
2021-11-15 14:47:51 +0800

    Finalized project page, removed images, fixed the formatting

---
+++ src/util/data.tsx
@@ -0,0 +1,15 @@
+export const postNames = [
+    "Portland, ME",
+    "Seattle, WA",
+    "Portland, OR",
+    "Riverside, CA",
+    "Seattle, WA",
+    "New York City, NY",
+    "New York City, NY",
+    "Boston, MA",
+    "Lousiana, NO"
+];
+
+export const IMAGE_SOURCE = "res/img";
+
+export const MARKDOWN_EXAMPLE = ""
